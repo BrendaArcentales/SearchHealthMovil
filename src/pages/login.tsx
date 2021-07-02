af24c91f4656e911249f7135ab5ef27a9827df33
Carlos Maldonado <kardaniel1998@gmail.com>
2021-07-02 00:19:21 -0500

    Uso de un AuthProvider

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,54 +1,84 @@
-import { IonCard, IonButton, IonContent, IonPage, IonHeader, IonToolbar, IonTitle,IonInput } from '@ionic/react';
-import { IonGrid, IonRow, IonCol} from '@ionic/react';
-import './Home.css';
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { loginUser } from '../firebaseConfig';
-import { toast } from '../toast';
+import {
+  IonCard,
+  IonButton,
+  IonContent,
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonInput,
+} from "@ionic/react";
+import { IonGrid, IonRow, IonCol } from "@ionic/react";
+import "./Home.css";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
+import { toast } from "../toast";
+import React from "react";
+import AuthProvider from "../services/auth";
+import { useHistory } from "react-router";
 const Login: React.FC = () => {
-  const [usermail,setUsermail]=useState('');
-  const [userpassword,setUserpassword]=useState('');
-  
-  async function login(){
-    if(usermail.trim() === '' || userpassword.trim() === ''){
-      return toast ('Los campos son obligatorios','danger')
+  const [usermail, setUsermail] = useState("");
+  const [userpassword, setUserpassword] = useState("");
+  const { login } = React.useContext(AuthProvider);
+  const history = useHistory();
+
+  const onlogin = async () => {
+    console.log(usermail);
+    if (usermail.trim() === "" || userpassword.trim() === "") {
+      return toast("Los campos son obligatorios", "danger");
     }
-    const res= await loginUser(usermail,userpassword)
-    if(!res){
-      toast('La combinación de correo y contraseña es erronea','danger')
-    }else{
-      toast('Acceso concedido','success')
+    const res = await login(usermail, userpassword);
+    if (!res) {
+      toast("La combinación de correo y contraseña es erronea", "danger");
+    } else {
+      toast("Acceso concedido", "success");
+      history.replace("/");
     }
-    console.log(`${res ? "Acceso concedido" :"Acceso denegado"}`)
-  }
+    console.log(`${res ? "Acceso concedido" : "Acceso denegado"}`);
+  };
 
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-        <IonTitle>Search Health-Login</IonTitle>
+          <IonTitle>Search Health-Login</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent>
+        <div className="ion-text-center">  
         <IonCard>
-          <IonInput placeholder="Email" color="tertiary" type="email" clearInput
-          onIonChange={(e: any)=>setUsermail(e.target.value)}/>
-          <IonInput placeholder="Contraseña" color="tertiary" type="password" clearInput
-          onIonChange={(e: any)=>setUserpassword(e.target.value)}/>
-           <IonGrid>
-              <IonRow>
-                <IonCol size="7" >
-                <IonButton  size="small" onClick={login} color="secondary">Iniciar Sesión</IonButton>
-                </IonCol>
-                <IonCol  size="5">
-                <IonButton size="small" routerLink="/" color="light">Volver</IonButton>
-                </IonCol>
-              </IonRow>
-         </IonGrid>
-          <p>Aún no tienes cuenta? <Link to="/register">Crear cuenta</Link></p>
-          <p><Link to="/recover">Olvidaste tu contraseña </Link></p>
+          <IonInput
+            placeholder="Email"
+            color="tertiary"
+            type="email"
+            clearInput
+            onIonChange={(e: any) => setUsermail(e.target.value)}
+          />
+          <IonInput
+            placeholder="Contraseña"
+            color="tertiary"
+            type="password"
+            clearInput
+            onIonChange={(e: any) => setUserpassword(e.target.value)}
+          />
+          <IonGrid>
+            <IonRow>
+              <IonCol >
+                <IonButton size="small" onClick={onlogin} color="secondary">
+                  Iniciar Sesión
+                </IonButton>
+              </IonCol>
+            </IonRow>
+          </IonGrid>
+          <p>
+            Aún no tienes cuenta? <Link to="/register">Crear cuenta</Link>
+          </p>
+          <p>
+            <Link to="/recover">Olvidaste tu contraseña </Link>
+          </p>
         </IonCard>
+        </div>
       </IonContent>
     </IonPage>
   );
